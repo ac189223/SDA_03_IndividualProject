3526e4400ef51d058d560dc64fc1fe7d6179788e
andrzejcalka <ac189223@o2.pl>
2019-10-18 04:04:57 +0200

    AddedJUnitTests

---
+++ src/test/java/IP_07/Model/RegisterTest.java
@@ -0,0 +1,92 @@
+package IP_07.Model;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RegisterTest {
+
+    @Test
+    void randomId() {
+    }
+
+    @Test
+    void addTaskToProject() {
+    }
+
+    @Test
+    void removeTaskFromProject() {
+    }
+
+    @Test
+    void removeAllTasksFromProject() {
+    }
+
+    @Test
+    void printStatus() {
+    }
+
+    @Test
+    void addTask() {
+    }
+
+    @Test
+    void markTaskAsDone() {
+    }
+
+    @Test
+    void findTask() {
+    }
+
+    @Test
+    void removeTask() {
+    }
+
+    @Test
+    void setTaskStatus() {
+    }
+
+    @Test
+    void setTaskDueDate() {
+    }
+
+    @Test
+    void setTaskTitle() {
+    }
+
+    @Test
+    void addProject() {
+    }
+
+    @Test
+    void markProjectAsDoneDependent() {
+    }
+
+    @Test
+    void markProjectAsDoneAlways() {
+    }
+
+    @Test
+    void findProject() {
+    }
+
+    @Test
+    void removeProjectAlways() {
+    }
+
+    @Test
+    void removeProjectDependent() {
+    }
+
+    @Test
+    void setProjectStatus() {
+    }
+
+    @Test
+    void setProjectDueDate() {
+    }
+
+    @Test
+    void setProjectTitle() {
+    }
+}
